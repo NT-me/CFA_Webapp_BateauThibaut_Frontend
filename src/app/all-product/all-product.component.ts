276b67172a05d75a122518d7a01003b5f3b83fe6
ratchet9364 <axel.bouvier@outlook.fr>
2021-04-19 10:22:35 +0200

    récupération tout les produits + tableaux

--- src/app/all-product/all-product.component.ts
+++ src/app/all-product/all-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'app-all-product',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllProductComponent implements OnInit {
 
-  constructor() { }
+  products: any;
+  poissons: any;
+  coquillages: any;
+  crustaces: any;
+
+  headers: any;
+  categories: any
+  constructor(public productsService : ProductsService) { 
+    this.products = [];
+    this.poissons = [];
+    this.coquillages = [];
+    this.crustaces = [];
+
+    this.headers = ["Nom", "Prix", "Prix en promotion", "Pourcentage de promotion", "Quantité en stock", "Commentaires"];
+    this.categories = ["Poissons", "Coquillages", "Crustacés"]
+  }
 
   ngOnInit(): void {
+    this.productsService.getInfoAllProducts().subscribe(data => {
+      this.products = data;
+    },
+    (err) => {
+      alert('failed');
+    });
+    this.productsService.getInfoAllPoissonsProducts().subscribe(data => {
+      this.poissons = data;
+    },
+    (err) => {
+      alert('failed');
+    });
+    this.productsService.getInfoAllCoquillagesProducts().subscribe(data => {
+      this.coquillages = data;
+    },
+    (err) => {
+      alert('failed');
+    });
+    this.productsService.getInfoAllCrustacesProducts().subscribe(data => {
+      this.crustaces = data;
+    },
+    (err) => {
+      alert('failed');
+    });
   }
 
 }
