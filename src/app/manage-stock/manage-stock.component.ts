0bebd6f83e96f97f6e1d288476d7595f4f67bcb1
Axel <silver-bird@hotmail.fr>
2021-04-20 16:39:52 +0200

    Manage stock update

--- src/app/manage-stock/manage-stock.component.ts
+++ src/app/manage-stock/manage-stock.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ManageProductsService } from '../services/manage-products.service';
 import { ProductsService } from '../services/products.service';
 
 @Component({
@@ -12,16 +13,22 @@ export class ManageStockComponent implements OnInit {
   poissons: any;
   coquillages: any;
   crustaces: any;
+  
+  ajoutStock: any;
+  discount: any;
 
   headers: any;
-  categories: any
-  constructor(public productsService : ProductsService) { 
+  categories: any;
+
+  constructor(public productsService : ProductsService, public manageProduct : ManageProductsService) { 
     this.products = [];
     this.poissons = [];
     this.coquillages = [];
     this.crustaces = [];
+    this.ajoutStock = [];
+    this.discount = [];
 
-    this.headers = ["Nom", "Prix", "Prix en promotion", "Pourcentage de promotion", "Quantité en stock", "Commentaires"];
+    this.headers = ["Nom", "Prix", "Prix en promotion", "Pourcentage de promotion", "Quantité en stock", "Commentaires","Ajouter Stock","Ajouter Promo"];
     this.categories = ["Poissons", "Coquillages", "Crustacés"]
   }
 
@@ -52,4 +59,28 @@ export class ManageStockComponent implements OnInit {
     });
   }
 
+  sendManageStock(){
+    console.log(this.discount)
+    let json = [{
+      "id": 1,
+      "stock": {
+        "quantity": 11,
+        "price": 0,
+        "type": "A"
+      },
+      "discPer": 15
+    },]
+    this.manageProduct.patchManageAll(json)
+    .subscribe(
+          (val) => {
+              console.log("PATCH call successful value returned in body", 
+                          val);
+          },
+          response => {
+              console.log("PATCH call in error", response);
+          },
+          () => {
+              console.log("The PATCH observable is now completed.");
+          }); ;
+  }
 }
