af5170e62b9aae4cf46466876b698cf9a395d9c4
Axel <silver-bird@hotmail.fr>
2021-04-22 09:55:48 +0200

    Bi update, displays transactions, filters are the nex step

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DetailsProductComponent } from './details-product/details-product.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule , ReactiveFormsModule} from '@angular/forms';
 import { AllProductComponent } from './all-product/all-product.component';
 import { ManageStockComponent } from './manage-stock/manage-stock.component';
 import { HomeComponent } from './home/home.component';
@@ -24,7 +23,8 @@ import { BiComponent } from './bi/bi.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule      
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/bi/bi.component.ts
+++ src/app/bi/bi.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { BiService } from '../services/bi.service';
 
 @Component({
@@ -9,19 +10,71 @@ import { BiService } from '../services/bi.service';
 export class BiComponent implements OnInit {
 
   allHistory: any;
-  total: any
+  transactions: any;
+  total: any;
+  headers: any;
+  dateFin: any;
+  dateDebut: any;
+  category : any;
+  type : any;
+  filters = new FormGroup({
+    category: new FormControl(''),
+    type: new FormControl(''),
+    dateDebut : new FormControl(null, [
+      Validators.required
+  ]),
+    dateFin : new FormControl(null, [
+      Validators.required
+  ])
+ });
+  
 
-  constructor(public biService : BiService) {
+
+  constructor(public biService : BiService, private fb: FormBuilder) {
       this.allHistory = [];
+      this.transactions =[];
+      this.headers = ["Date","Nom","Quantité","Type transaction","Gain","Perte"]
   }
 
   ngOnInit(): void {
     this.biService.getInfoAllHistory().subscribe(data => {
       this.allHistory = data;
+      this.transactions = data["transactions"];
     },
     (err) => {
       alert('failed');
     });
   }
 
+  convertTimeStamp(timeStamp): String {
+    var date = new Date(timeStamp*1000);
+    return (date.getDate())+"-"+(date.getMonth()+1)+"-"+date.getFullYear()+"-"+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+  }
+
+  checkTypeC(timeStamp): String {
+    if(timeStamp.type == 'A'){
+      return timeStamp.totalPrice
+    }
+    else 
+      return '0';
+  }
+
+  checkTypeD(timeStamp): String {
+    if(timeStamp.type == 'RPV'){
+      return timeStamp.totalPrice
+    }
+    else 
+      return '0';
+  }
+
+  convertToTimeStamp(date): number {
+    console.log(Date.parse(date)/1000);
+    return (Date.parse(date)/1000);
+  }
+
+  submitForm() {
+    if(this.filters.value)
+    this.biService.getInfosFiltered
+    console.log(this.filters.value)
+  }
 }
