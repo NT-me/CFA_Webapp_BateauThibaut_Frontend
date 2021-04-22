eb69a08a4add4cfa6fcbbd8a7bcb2591b7782aba
gnouf1 <jeuxforum26@gmail.com>
2021-04-23 01:18:01 +0200

    formulaire auth --> :+1:

---
+++ src/app/services/authentification.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthentificationService {
+    requestOptions
+    apiTokenReq
+
+    constructor(public http: HttpClient) {
+      this.requestOptions = new Headers({
+        'accept':'application/json',
+        'Content-Type': 'application/json'
+      });
+      this.apiTokenReq = "http://localhost:8000/security/pswd/token";
+  }
+
+    retrieveToken(formdata: any){
+      console.log(this.requestOptions);
+      return this.http.post(this.apiTokenReq, formdata, this.requestOptions);
+  }
+}
